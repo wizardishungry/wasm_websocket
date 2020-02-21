7ee9023657063b30d14bb5a43fc50f3e8f3a8567
Jon Williams <jon@jonwillia.ms>
2020-02-20 20:49:59 -0500

    no parallel tests

--- wasm_test.go
+++ wasm_test.go
@@ -4,8 +4,9 @@ import (
 	"testing"
 )
 
+// avoid t.Parallel https://travis-ci.community/t/goos-js-goarch-wasm-go-run-fails-panic-newosproc-not-implemented/1651
+
 func TestMustGlobal(t *testing.T) {
-	t.Parallel()
 	ws := Must(Global(WebSocketArgs{url: "wss://test.example.com/ws"}))
 	if ws == nil {
 		t.Fatalf("nil returned by Must")
@@ -13,7 +14,6 @@ func TestMustGlobal(t *testing.T) {
 }
 
 func TestDoesntPanicOnConstructorError(t *testing.T) {
-	t.Parallel()
 	ws, err := Global(WebSocketArgs{url: "http://test.example.com/ws"})
 	if err == nil {
 		t.Fatalf("nil error returned by Global")
